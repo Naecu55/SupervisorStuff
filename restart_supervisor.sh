7d9dbf20e7f1c6bd6193921d4c2376a798287338
Chuckie2 <amadi.chuck@gmail.com>
2017-07-12 08:07:17 +0100

    Initial Config

---
+++ restart_supervisor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# As root
+#
+# Created by Chuck on Thu Dec 22 09:03:16 GMT 2016
+# Run supervisor script and email supervisord status
+
+echo "$(date) Change Directory to supervisor.d"
+cd /etc/supervisord.d/
+echo "Stop supervisor.d Service"
+service supervisord stop
+echo "Start supervisor using the appropriate supervisord conf file"
+supervisord -c supervisord.conf
+echo "sleep for 5 seconds before checking supervisorctl status"
+sleep 5;
+supervisorctl status | mailx -s "Cloud Supervisord workers status" amadi.chuck@gmail.com
+echo "Check that workers are running from the email notification!"
+supervisorctl status
